2d694cdb90c1fa7a5e3f0458c54827c091029c69
azandrew-sidoine <azandrewdevelopper@gmail.com>
2025-07-29 11:38:55 +0200

    Added providers to the public API

--- projects/ngx-storage/src/public-api.ts
+++ projects/ngx-storage/src/public-api.ts
@@ -6,10 +6,17 @@ export { StorageInterface, ModuleConfig } from './lib/types';
 export { InMemoryStorage } from './lib/in-memory';
 export { SessionStorage } from './lib/session';
 export { LocalStorage } from './lib/local';
+
 export {
   DOCUMENT_LOCAL_STORAGE,
   DOCUMENT_SESSION_STORAGE,
   DEFAULT_STORAGE,
   FAKE_STORAGE,
-  IN_MEMORY_STORAGE
+  IN_MEMORY_STORAGE,
 } from './lib/tokens';
+
+export {
+  provideInMemoryStorage,
+  provideLocalStorage,
+  provideSessionStorage,
+} from './lib/providers';
